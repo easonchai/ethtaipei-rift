f3458c957065a102ac8cb231c4f9279acd5879f4
Eason Chai <echai2905@gmail.com>
2024-03-23 15:15:17 +0800

    feat: Add logo

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,6 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center px-4 lg:px-24 py-2 bg-cover bg-center object-fill text-rift-grey-900">
+      <Image
+        src={"/assets/logo.svg"}
+        alt="Rift"
+        width={120}
+        height={120}
+        className="mb-10"
+      />
       <div className="flex flex-row items-center justify-center gap-5">
         <div className="border-2 border-rift-grey-900 flex-1 flex flex-col">
           <div className="h-8 w-full flex flex-row px-2 py-5 items-end gap-x-4 bg-rift-yellow-2 border-b-2 border-rift-grey-900">
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -8,10 +8,33 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Trebuchet MS", "ui-sans-serif", "system-ui"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        cover: 'url("/assets/background.png")',
+      },
+      colors: {
+        rift: {
+          yellow: {
+            1: "#FFF7CC",
+            2: "#FED0A2",
+            3: "#FFAB9D",
+          },
+          purple: {
+            1: "#FFE4F4",
+            2: "#F5ACFE",
+            3: "#C69FFF",
+          },
+          grey: {
+            500: "#DFDFDF",
+            700: "#848484",
+            900: "#141400",
+          },
+        },
       },
     },
   },
